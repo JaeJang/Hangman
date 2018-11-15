01585c9acc3eeb0894b4bc667c49d54f90e6ae57
Jae Jang <jaejang5566@gmail.com>
2018-11-14 23:05:18 -0800

    db environment

--- config/database.js
+++ config/database.js
@@ -1,7 +1,17 @@
-module.exports={
+var local = {
     'host':'localhost',
     'user':'root',
-    'password':'1111',
+    'password':'123a456',
+    'port':'3306',
+    'database':'hangman_test'
+}
+
+var public = {
+    'host':'localhost',
+    'user':'root',
+    'password':'123a456',
     'port':'3306',
     'database':'hangman'
 }
+
+module.exports = (process.argv[2] =='public' || process.argv[3] =='public') ? public : local;
